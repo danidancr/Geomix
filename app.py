a0550181614632e6228d0617f8ba54310278e1b9
danidancr <129102611+danidancr@users.noreply.github.com>
2026-04-21 15:25:19 -0400

    Update app
    Refactor main block to use environment variable for port.

--- app.py
+++ app.py
@@ -412,7 +412,11 @@ def api_profile_update():
 def strptime_filter(value, fmt):
     return datetime.strptime(value, fmt)
 
-if __name__ == '__main__':
+import os
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
